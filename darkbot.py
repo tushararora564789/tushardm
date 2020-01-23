6990a50f5c4a76065cdb2c8ddd0fe3ca9ae304bf
tushararora564789 <60184292+tushararora564789@users.noreply.github.com>
2020-01-23 16:29:51 +0000

    Update darkbot.py

--- darkbot.py
+++ darkbot.py
@@ -19,11 +19,11 @@ client.remove_command('help')
 
 async def status_task():
     while True:
-        await client.change_presence(game=discord.Game(type=1,name='with TRIVIA NATION 2.0'))
+        await client.change_presence(game=discord.Game(type=1,name='with ᏒᎬᎪᏢᎬᏒ || ᎾffᎥᏟᎥᎪᏞ'))
         await asyncio.sleep(5)
         await client.change_presence(game=discord.Game(type=1,name='Joined Till Now '+str(len(set(client.get_all_members())))+' users'))
         await asyncio.sleep(5)
-        await client.change_presence(game=discord.Game(type=1,name='with KESHAV RAJ ᴳᵒᵈ'))
+        await client.change_presence(game=discord.Game(type=1,name='with ⎝⧹𝗗𝗿. HUNTER™╱⎠'))
         await asyncio.sleep(5)
         
 
@@ -33,8 +33,8 @@ async def on_ready():
     print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     print('--------')
     print('--------')
-    print('Started TRIVIA NATION 2.O')
-    print('Created by KESHAV RAJ ᴳᵒᵈ')
+    print('Started ᏒᎬᎪᏢᎬᏒ || ᎾffᎥᏟᎥᎪᏞ')
+    print('Created by ⎝⧹𝗗𝗿. HUNTER™╱⎠')
     client.loop.create_task(status_task())
 
 
@@ -48,10 +48,10 @@ async def on_member_join(member):
     r, g, b = tuple(int(x * 255) for x in colorsys.hsv_to_rgb(random.random(), 1, 1))
     embed = discord.Embed(color = discord.Color((r << 16) + (g << 8) + b))
     embed.set_author(name='Welcome message')
-    embed.add_field(name = '**__Welcome to Our Server TRIVIA NATION 2.0__**',value ='**Hope you will be active here. Check Our server <#610816633287606274> and never try to break any rules**. ',inline = False)
+    embed.add_field(name = '**__Welcome to Our Server ᏒᎬᎪᏢᎬᏒ || ᎾffᎥᏟᎥᎪᏞ**',value ='**Hope you will be active here. Check Our server <#610816633287606274> and never try to break any rules**. ',inline = False)
     embed.set_image(url = 'https://cdn.discordapp.com/attachments/610828452442013737/610897950201217048/390.gif')
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f"created by KESHAV RAJ ᴳᵒᵈ#7735",icon_url="https://cdn.discordapp.com/attachments/609581567135842314/609937606964281365/JPEG_20190811_080005.jpg")
+    embed.set_footer(text=f"created by ⎝⧹𝗗𝗿. HUNTER™╱⎠",icon_url="https://cdn.discordapp.com/attachments/609581567135842314/609937606964281365/JPEG_20190811_080005.jpg")
     await client.send_message(member,embed=embed)
     print("Sent message to " + member.name)
     channel = discord.utils.get(client.get_all_channels(), server__name='TRIVIA NATION 2.0', name='welcome')
@@ -61,8 +61,8 @@ async def on_member_join(member):
     embed.add_field(name='Your join position is', value=member.joined_at)
     embed.set_image(url = 'https://cdn.discordapp.com/attachments/610828452442013737/610898014059495424/tenor.gif')
     embed.set_thumbnail(url=member.avatar_url)
-    embed.set_footer(text=f"created by KESHAV RAJ ᴳᵒᵈ#7735",icon_url="https://cdn.discordapp.com/attachments/609581567135842314/609937606964281365/JPEG_20190811_080005.jpg")
+    embed.set_footer(text=f"created by ⎝⧹𝗗𝗿. HUNTER™╱⎠",icon_url="https://cdn.discordapp.com/attachments/609581567135842314/609937606964281365/JPEG_20190811_080005.jpg")
     await client.send_message(channel, embed=embed)
 
 
-client.run('NjEwMTc0NzU0Nzc5MDM3Njk2.XVDwuw.alVmHrb6uJi6LTuZsjNQelY_yqE')
+client.run('NjY5OTM5NDI1ODY5OTU1MDgz.XinIWg.9T5qbjHgztGJUQ9Xw9e5GDi-IAY')
